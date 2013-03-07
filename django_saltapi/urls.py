cdd6c1789d3da0915d8b44135fe9d42f608a95af
Henrik Holmboe <henrik@holmboe.se>
2013-03-07 17:07:24 +0100

    The regex was too limited for a hostname.

--- django_saltapi/urls.py
+++ django_saltapi/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('django_saltapi.views',
     url(r'^$', 'apiwrapper'),
-    url(r'^ping/(?P<tgt>[a-zA-Z0-9.-]+)/$', 'ping'),
-    url(r'^echo/(?P<tgt>[a-zA-Z0-9.-]+)/(?P<arg>\w+)/$', 'echo'),
+    url(r'^ping/(?P<tgt>\w+)/$', 'ping'),
+    url(r'^echo/(?P<tgt>\w+)/(?P<arg>\w+)/$', 'echo'),
 )
