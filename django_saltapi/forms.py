c9925fc0a74a63ea10a9dd96b7876a1a48be7249
Henrik Holmboe <henrik@holmboe.se>
2013-03-11 16:22:26 +0100

    Whitespace.

--- django_saltapi/forms.py
+++ django_saltapi/forms.py
@@ -6,13 +6,13 @@ class LowdataForm(forms.Form):
     client = forms.CharField(max_length=20,
                              initial='local',
                              widget=forms.HiddenInput())
-    tgt = forms.CharField(max_length=50,
-                          label='Target')
-    fun = forms.CharField(max_length=30,
-                          label='Module')
-    arg = forms.CharField(max_length=100,
-                          required=False,
-                          label='Arguments')
+    tgt    = forms.CharField(max_length=50,
+                             label='Target')
+    fun    = forms.CharField(max_length=30,
+                             label='Module')
+    arg    = forms.CharField(max_length=100,
+                             required=False,
+                             label='Arguments')
 
     def clean(self):
         cleaned_data = super(LowdataForm, self).clean()
