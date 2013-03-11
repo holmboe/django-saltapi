bb283d2e74c543ece53bca2c13a82e56fcea5e79
Henrik Holmboe <henrik@holmboe.se>
2013-03-11 16:08:21 +0100

    Add regex constants for tagets and jids.

--- django_saltapi/urls.py
+++ django_saltapi/urls.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from django_saltapi.utils import REGEX_JID, REGEX_HOSTNAME
+
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('django_saltapi.views',
     url(r'^$', 'apiwrapper'),
 
     url(r'^minions/$', 'minions', {'mid': None}),
-    url(r'^minions/(?P<mid>\w+)/$', 'minions'),
+    url(r'^minions/(?P<mid>' + REGEX_HOSTNAME + ')/$', 'minions'),
 
     url(r'^jobs/$', 'jobs', {'jid': None}),
-    url(r'^jobs/(?P<jid>\d+)/$', 'jobs'),
+    url(r'^jobs/(?P<jid>' + REGEX_JID + ')/$', 'jobs'),
 
-    url(r'^ping/(?P<tgt>\w+)/$', 'ping'),
-    url(r'^echo/(?P<tgt>\w+)/(?P<arg>\w+)/$', 'echo'),
+    url(r'^ping/(?P<tgt>' + REGEX_HOSTNAME + ')/$', 'ping'),
+    url(r'^echo/(?P<tgt>' + REGEX_HOSTNAME + ')/(?P<arg>\w+)/$', 'echo'),
 )
---
+++ django_saltapi/utils.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# http://stackoverflow.com/questions/106179/regular-expression-to-match-hostname-or-ip-address
+REGEX_IPADDR   = "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
+REGEX_HOSTNAME = "(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])"
+REGEX_JID      = "([0-9]{20})"
